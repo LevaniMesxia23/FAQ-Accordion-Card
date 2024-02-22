7e3998417bcb7c1a305b614325a35529e29d61d7
levan mesxia <levanmesxia@levans-MacBook-Pro.local>
2024-02-22 22:17:29 +0400

    update design

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const questions = document.querySelectorAll(".sentence-box")
 
 questions.forEach(elem =>
     elem.addEventListener("click", () => {
-        elem.classList.toggle("open")
+        elem.classList.toggle("open");
 
         questions.forEach(otherElem => {
             if (otherElem !== elem) {
-                otherElem.classList.remove("open")
+                otherElem.classList.remove("open");
                 setTimeout(() => {
                     otherElem.classList.add("close");
                 }, 2000);
